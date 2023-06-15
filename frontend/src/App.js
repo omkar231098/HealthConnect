b6f147db43431de0f13f63a2458558e7dd6d88c0
sandeepguptax2003 <sandeepguptax2003@gmail.com>
2023-06-15 22:24:59 +0530

    DAY3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext } from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Home from "./Components/Home/Home/Home";
+import SignInForm from "./components/Login/LoginMain/SignInForm";
+import Dashboard from "./components/Dashboard/Dashboard/Dashboard";
+import AppointMent from "./components/AppointMent/AppointMent/AppointMent";
+import AllPatients from "./components/AppointMent/AllPatients/AllPatients";
+import AddDoctor from "./components/Dashboard/AddDoctor/AddDoctor";
+import DoctorList from "./components/Dashboard/DoctorList/DoctorList";
+import AddReview from "./components/Dashboard/My Review/AddReview";
+import PrivateRoute from "./components/Login/PrivateRoute/PrivateRoute";  
+
+export const UserContext = createContext();
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/login", element: <SignInForm /> },
+  { path: "/dashboard", element: <Dashboard /> },
+  { path: "/patients", element: <AllPatients /> },
+  { path: "/addDoctor", element: <AddDoctor /> },
+  { path: "/doctors", element: <DoctorList /> },
+  { path: "/auth/review", element: <AddReview /> },
+  {
+    path: "/appointment",
+    element: (
+      <PrivateRoute>
+        <AppointMent />
+      </PrivateRoute>
+    ),
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
-
 export default App;
