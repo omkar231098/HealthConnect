a2602dbd07524e6e50659ce4d59b1968b45863ba
ShradhaVastrakar <shradhavastrakar@gmail.com>
2023-06-16 22:31:47 +0530

    fw23_0771_DAY3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-      
+        
       </Routes>
     </BrowserRouter>
     </div>
