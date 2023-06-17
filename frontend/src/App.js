48a68e46554e9092a7d482b8ea44ff82594f7958
Omkar Dhanave <o.m.dhanave@gmail.com>
2023-06-17 19:10:03 +0530

    add paymenthistory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,8 @@ import Login from "./Components/Login";
 import PersonalDetails from "./pages/PaitentDash";
 import DoctorDashboard from "./pages/DoctorDash";
 import PersonalDetailsDocter from "./pages/PersonalDetails";
-
+import TodaysSchedule from "./Components/TodaysSchedule";
+import DocAppointments from "./pages/PaymentHistory";
 function App() {
   return (
     <div className="App">
@@ -16,6 +17,8 @@ function App() {
             <Route path="/paitentDash" element ={<PersonalDetails />} />
             <Route path="/doctorDash" element ={<DoctorDashboard />} />
             <Route path="/doctor/perosnaldetails" element ={<PersonalDetailsDocter />} />
+            <Route path="/doctor" element ={<TodaysSchedule/>} />
+            <Route path="/doctor/payment-history" element ={<DocAppointments/>} />
           </Routes>
     </div>
   );
