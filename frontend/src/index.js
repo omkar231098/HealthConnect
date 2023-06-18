e6f9993cdb7158e7118d4ab5c162ad9d77f7b902
sandeepguptax2003 <sandeepguptax2003@gmail.com>
2023-06-18 21:52:40 +0530

    EDITED INDEX.JS AND PACKAGE FILES

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,35 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {store} from './redux/store'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import AuthContextProvider from "./Context/AuthContext";
 import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from "@chakra-ui/react";
 // import { ReactReduxContext } from 'react-redux';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
   <BrowserRouter>
-  
     <ChakraProvider>
       <AuthContextProvider>
-
-      <Provider store={store}>
- 
+        <Provider store={store}>
           <App />
-</Provider>
-
-          < App />
-
+        </Provider>
       </AuthContextProvider>
     </ChakraProvider>
-
   </BrowserRouter>
-
 );
 
 // If you want to start measuring performance in your app, pass a function
